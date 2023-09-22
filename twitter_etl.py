44f177cbc98553042848e9321890db85767265a6
samar.mami18@gmail.com <samar.mami18@gmail.com>
2023-09-22 19:22:34 +0200

    env variables + tweepy auth

--- twitter_etl.py
+++ twitter_etl.py
@@ -5,15 +5,20 @@ from datetime import datetime
 import s3fs
 import os
 from dotenv import load_dotenv
-load_dotenv()
+
 # Load environment variables
+load_dotenv()
 consumer_key = os.getenv("CONSUMER_KEY")
 consumer_secret = os.getenv("CONSUMER_SECRET")
 access_token = os.getenv("ACCESS_TOKEN")
 access_token_secret = os.getenv("ACCESS_TOKEN_SECRET")
 
+# Twitter authentification
 auth = tweepy.OAuth1UserHandler(
     consumer_key, consumer_secret, access_token, access_token_secret
 )
 
+# Creating an API object
 api = tweepy.API(auth)
+api.user_timeline(user_id='@elonmusk',
+                  count=200)
